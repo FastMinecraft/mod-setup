49096d29631b01013e4971f182faa37fbd7fc2cb
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-07-04 16:26:00 -0400

    Fixed genSources ordering

--- src/main/kotlin/dev/fastmc/modsetup/ArchitecturyRootConfigure.kt
+++ src/main/kotlin/dev/fastmc/modsetup/ArchitecturyRootConfigure.kt
@@ -1,11 +1,12 @@
 package dev.fastmc.modsetup
 
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.jetbrains.kotlin.gradle.plugin.extraProperties
-import java.io.File
 
 class ArchitecturyRootConfigure(project: Project) : ProjectConfigure("architecturyRoot", project) {
     val projectExtension = project.extensions.create("architecturyProject", ArchitecturyProjectExtension::class.java)
+    private val flag = BooleanArray(project.allprojects.size) { false }
 
     override fun configure() {
         project.pluginManager {
@@ -61,6 +62,53 @@ class ArchitecturyRootConfigure(project: Project) : ProjectConfigure("architectu
 
         project.subprojects {
             ArchitecturySubprojectConfigure(it).configure()
+        }
+
+        project.allprojects.forEachIndexed { i, it ->
+            it.afterEvaluate {
+                synchronized(flag) {
+                    flag[i] = true
+                    if (!flag.contains(false)) {
+                        afterAll()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun afterAll() {
+        val subprojects = project.subprojects.sortedByDescending { it.name }
+
+        fun getTasks(taskName: String): List<Task> {
+            return subprojects.mapNotNull {
+                it.tasks.findByName(taskName)
+            }
+        }
+
+        val unpickJar = getTasks("unpickJars")
+        val genSourcesWithCfr = getTasks("genSourcesWithCfr")
+        val genSourcesWithFernFlower = getTasks("genSourcesWithFernFlower")
+
+
+        fun foldTasks(tasks: List<Task>) {
+            tasks.fold(null as Task?) { prev, task ->
+                if (prev != null) {
+                    task.mustRunAfter(prev)
+                }
+                task
+            }
+        }
+
+        foldTasks(unpickJar)
+        foldTasks(genSourcesWithCfr)
+        foldTasks(genSourcesWithFernFlower)
+
+        genSourcesWithCfr.forEach {
+            it.mustRunAfter(unpickJar)
+        }
+
+        genSourcesWithFernFlower.forEach {
+            it.mustRunAfter(unpickJar)
         }
     }
 }
